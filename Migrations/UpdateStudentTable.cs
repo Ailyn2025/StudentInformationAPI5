90db1af88d212a1bf0044a30b9c693f6f9a16eab
Ailyn <aiasanchez1989@gmail.com>
2026-03-22 19:20:05 +0800

    Update Students table schema with new columns
    Renamed columns in the Students table and added new columns for BirthDate, FirstName, Gender, and LastName.

---
+++ Migrations/UpdateStudentTable.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace studentAPI2.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStudentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Students",
+                newName: "StudentNo");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Students",
+                newName: "StudentId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Students",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Students");
+
+            migrationBuilder.RenameColumn(
+                name: "StudentNo",
+                table: "Students",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "StudentId",
+                table: "Students",
+                newName: "Id");
+        }
+    }
+}
